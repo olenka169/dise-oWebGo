85e289fc2ca6121d8ce5626233750188516f5695
olenka169 <olenkagonzalesvasquez@gmail.com>
2021-10-26 09:42:53 -0500

    Respaldo26.10

---
+++ Sesion21/js/agendaV1.js
@@ -0,0 +1,12 @@
+function guardardatos(){
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.movil = document.getElementById("movil").value;
+}
+
+function recuperardatos(){
+    if ((localStorage.nombre != undefined) && (localStorage.movil != undefined)) {
+        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br> Num. celular: " + localStorage.movil;
+    } else {
+        document.getElementById("datos").innerHTML = "No has ingresado tus datos en la agenda";
+    }
+}
